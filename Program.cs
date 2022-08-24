9de3834f1b291854321db5603172a4ceabdd8543
kotableOrKisaki <super_misha05@mail.ru>
2022-08-24 20:08:26 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace thirdTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Как тебя зовут? \t");
+            string Name = Console.ReadLine();
+
+            Console.Write("Сколько лет? \t");
+            string Age = Console.ReadLine();
+
+            Console.Write("Где живешь? \t");
+            string City = Console.ReadLine();
+
+            Console.WriteLine($"Твое имя {Name}, сейчас тебе {Age}, и ты живешь в городе {City}");
+        }
+    }
+}
